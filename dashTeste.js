ed6b1e7917188f3c41c6cce1cd96a5f894f08156
giocarvalho07 <giovannidecarvalhomendes@outlook.com>
2019-12-05 11:14:11 -0200

    pagina administrador

--- dashTeste.js
+++ dashTeste.js
@@ -1,27 +1,27 @@
-$('.btn-expand-collapse').click(function(e) {
-    $('.navbar-primary').toggleClass('collapsed');
-});
+// $('.btn-expand-collapse').click(function(e) {
+//     $('.navbar-primary').toggleClass('collapsed');
+// });
 
-$(".slider").slick({
-    centerMode: true,
-    slidesToShow: 3,
-    dots: true,
-    autoplay: true,
-    autoplaySpeed: 1000,
-});
+// $(".slider").slick({
+//     centerMode: true,
+//     slidesToShow: 3,
+//     dots: true,
+//     autoplay: true,
+//     autoplaySpeed: 1000,
+// });
 
-function menu(id) {
-    if (document.getElementById(id).style.dispaly == "block") {
-        document.getElementById(id).style.dispaly = "none";
-    } else {
-        document.getElementById(id).style.dispaly = "block";
-    }
-}
+// function menu(id) {
+//     if (document.getElementById(id).style.dispaly == "block") {
+//         document.getElementById(id).style.dispaly = "none";
+//     } else {
+//         document.getElementById(id).style.dispaly = "block";
+//     }
+// }
 
-// ============= teste paginação
+// // ============= teste paginação
 
-// '.tbl-content' consumed little space for vertical scrollbar, scrollbar width depend on browser/os/platfrom. Here calculate the scollbar width .
-$(window).on("load resize ", function() {
-    var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width();
-    $('.tbl-header').css({'padding-right':scrollWidth});
-  }).resize();
+// // '.tbl-content' consumed little space for vertical scrollbar, scrollbar width depend on browser/os/platfrom. Here calculate the scollbar width .
+// $(window).on("load resize ", function() {
+//     var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width();
+//     $('.tbl-header').css({'padding-right':scrollWidth});
+//   }).resize();
